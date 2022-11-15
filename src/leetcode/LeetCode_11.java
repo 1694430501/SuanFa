c7b84e43a70e6b3b0f722854d3f7bec8d12e870c
wenzhan <1694430501@qq.com>
2022-11-15 19:03:44 +0800

    题干

---
+++ src/leetcode/LeetCode_11.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+/**
+ * @author: wz
+ * @date: 2022/11/15 0015
+ * Buddha Bless, No Bug !
+ */
+public class LeetCode_11 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/leetcode/LeetCode_14.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+ * @author: wz
+ * @date: 2022/11/15 0015
+ * Buddha Bless, No Bug !
+ *
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ *
+ * 如果不存在公共前缀，返回空字符串 ""。
+ *
+ * 示例 1：
+ * 输入：strs = ["flower","flow","flight"]
+ * 输出："fl"
+ *
+ * 示例 2：
+ * 输入：strs = ["dog","racecar","car"]
+ * 输出：""
+ * 解释：输入不存在公共前缀。
+ *
+ */
+public class LeetCode_14 {
+    public static void main(String[] args) {
+
+    }
+}
